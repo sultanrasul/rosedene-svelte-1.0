433953fa9e5d25fb6069e76d4b700a6817cd139a
Funky Panda <72228432+Funky-Panda@users.noreply.github.com>
2025-02-19 11:39:39 +0000

    uploading to server

--- svelte.config.js
+++ svelte.config.js
@@ -27,15 +27,14 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
 			fallback: "index.html",
-			precompress: false,
-			strict: true
 		}),
 		alias: {
 			"@/*": "./src/lib/*"
 		},
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '' : '',
+		}
 
 		
 	}
