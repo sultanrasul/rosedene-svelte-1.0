c1ef1a69fe106f9f02454eed43b4e093b5137bcd
Funky Panda <72228432+Funky-Panda@users.noreply.github.com>
2025-04-09 13:18:15 +0100

    fixed the padding on apartments

--- src/routes/components/apartments/Main.svelte
+++ src/routes/components/apartments/Main.svelte
@@ -37,7 +37,7 @@
 
 {#if browser}
 
-<div class="relative pt-24 px-14 w-full overflow-x-hidden bg-[#233441]">
+<div class="relative pt-24 px-6 sm-px-12 md:px-16 lg:px-22 w-full overflow-x-hidden bg-[#233441]">
       <!-- Background Layers -->
       <div class="absolute inset-0 z-0">
         <!-- Primary Background -->
