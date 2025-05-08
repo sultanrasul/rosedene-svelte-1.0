eb0d3d6ee47f6bdb0755e3dbf915693db7399004
Funky Panda <72228432+Funky-Panda@users.noreply.github.com>
2025-05-08 13:52:58 +0100

    forgot to change the backend url back

--- src/routes/conf.js
+++ src/routes/conf.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://127.0.0.1:8080";
-// export const BACKEND_URL = "https://core.rosedenedirect.com";
+// export const BACKEND_URL = "http://127.0.0.1:8080";
+export const BACKEND_URL = "https://core.rosedenedirect.com";
 export const PK = "pk_test_51QkSqLAQmfY2PDog1Sd4LCK6Y85GYEdbOP1CeVQ2iw7P9KJ362fbf4PzLq2cEJ4OYJSqrSSN7DI52SmStGCVS8Qa00C9oDsRb0";
 
