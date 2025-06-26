6d56faafa65c4f4947492f6aa210083f39341e40
Funky Panda <72228432+Funky-Panda@users.noreply.github.com>
2025-06-26 12:33:09 +0100

    Don't need to keep changing conf.js anymore
    Now I made it so that it works with env so I can edit that stuff based on if it is on production or not in cloudflare

--- src/routes/conf.js
+++ src/routes/conf.js
@@ -1,3 +1,5 @@
 // export const BACKEND_URL = "http://127.0.0.1:8080";
-export const BACKEND_URL = "https://core.rosedenedirect.com";
-export const PK = "pk_test_51QkSqLAQmfY2PDog1Sd4LCK6Y85GYEdbOP1CeVQ2iw7P9KJ362fbf4PzLq2cEJ4OYJSqrSSN7DI52SmStGCVS8Qa00C9oDsRb0";
+// export const PK = "pk_test_51QkSqLAQmfY2PDog1Sd4LCK6Y85GYEdbOP1CeVQ2iw7P9KJ362fbf4PzLq2cEJ4OYJSqrSSN7DI52SmStGCVS8Qa00C9oDsRb0";
+
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const PK = import.meta.env.VITE_PK;
